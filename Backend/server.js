8b5baad29da72de25e46dd97ab1ea7fd83112e51
Sumonta Garai <119773985+Darlingtons@users.noreply.github.com>
2025-02-19 20:32:19 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -22,11 +22,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
-app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.use(express.static(path.join(__dirname,"/Frontend/dist")));
 
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+    res.sendFile(path.join(__dirname,"Frontend","dist","index.html"));
 })
 
 server.listen(PORT,()=>{
